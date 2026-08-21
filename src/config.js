947136131e5f4d2f9acf1bb9c0d18bf9bcd5f864
hugo-sants <hugovictorsantos30@gmail.com>
2026-08-21 16:24:27 -0300

    chore: centralize rain clock defaults

---
+++ src/config.js
@@ -0,0 +1,31 @@
+export const DEFAULTS = {
+    position: 'center',
+    marginX: 60,
+    marginY: 80,
+    timeChar: '-',
+
+    day: {
+        baseSize: 86,
+        baseLetterSpacing: 20,
+        fontFamily: 'Anurati',
+    },
+
+    secondary: {
+        baseSize: 23,
+        baseLetterSpacing: 4,
+        datePaddingTop: 8,
+        timePaddingTop: 4,
+        fontFamily: 'Poppins',
+    },
+
+    scaling: {
+        baseHeight: 1080,
+    },
+
+    wallpaper: {
+        centerRegionSize: 0.20,
+        analysisSize: 100,
+        darkThreshold: 0.20,
+        lightThreshold: 0.80,
+    },
+};
